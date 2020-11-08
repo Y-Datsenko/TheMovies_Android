55022e8626e3dac9ef593ef2b5fed16bc102a160
Y-Datsenko <yevhenii.datsenko@gmail.com>
2020-11-08 14:03:10 +0200

    initial commit

---
+++ app/src/main/java/com/example/themovies/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.themovies
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.example.themovies.ui.TheMoviesTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TheMoviesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = name)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TheMoviesTheme {
+        Greeting("Android")
+    }
+}
