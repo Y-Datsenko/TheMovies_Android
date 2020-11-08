959a1e9d038b50b30d2b29a0ddec81f822e0e9b3
Y-Datsenko <yevhenii.datsenko@gmail.com>
2020-11-08 16:14:13 +0200

    added firstLetter representation

--- app/src/main/java/com/example/themovies/MainActivity.kt
+++ app/src/main/java/com/example/themovies/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.themovies
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.example.themovies.ui.TheMoviesTheme
 
@@ -26,7 +34,14 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun MovieItem(movie: Movie) {
-    Text(text = movie.title)
+    Box(
+        alignment = Alignment.Center,
+        modifier = Modifier
+            .size(size = 50.dp)
+            .background(color = Color.Green, shape = CircleShape)
+    ) {
+        Text(text = movie.firstLetter)
+    }
 }
 
 @Preview(showBackground = true)
