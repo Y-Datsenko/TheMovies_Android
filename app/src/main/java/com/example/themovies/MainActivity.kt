68d10e94ebac5e4b46b61a0e3bccd189ffe2ba09
Y-Datsenko <yevhenii.datsenko@gmail.com>
2020-11-12 19:11:07 +0200

    styled MovieItem

--- app/src/main/java/com/example/themovies/MainActivity.kt
+++ app/src/main/java/com/example/themovies/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -17,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.example.themovies.ui.TheMoviesTheme
@@ -42,13 +45,24 @@ fun MovieItem(movie: Movie) {
             alignment = Alignment.Center,
             modifier = Modifier
                 .size(size = 50.dp)
-                .background(color = Color.Green, shape = CircleShape)
+                .background(color = Color(61, 198, 94), shape = CircleShape)
         ) {
-            Text(text = movie.firstLetter)
+            Text(text = movie.firstLetter, color = Color.White)
         }
         Column(modifier = Modifier.padding(start = 8.dp)) {
-            Text(text = movie.title)
-            Text(text = movie.description)
+            Text(
+                text = movie.title,
+                style = MaterialTheme.typography.h6,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 2
+            )
+            Text(
+                text = movie.description,
+                style = MaterialTheme.typography.subtitle1,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 2,
+                modifier = Modifier.padding(top = 2.dp)
+            )
         }
     }
 }
